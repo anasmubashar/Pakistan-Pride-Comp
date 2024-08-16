cd6cfc6e23d86c2378c01017e791d16afa316db0
Haiderbukhari <apshaiderbukhari786@gmail.com>
2024-08-16 12:04:10 +0500

    home fixes

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="w-full h-[65vh] bg-[url('/Light.png')] text-white relative overflow-hidden">
+    <div className="w-full h-[65vh] min-h-screen bg-[url('/Light.png')] text-white relative overflow-hidden">
       <img
         className="absolute z-[1] w-full h-full object-contain"
         src="/Line.png"
@@ -22,17 +22,17 @@ export default function Hero() {
       <h1 className="text-center text-gradient text-6xl mt-24">
         Shaping the Future
       </h1>
-      <p className="mx-auto text-center w-[40%] text-[rgba(255, 255, 255, 0.70)] mt-6">
+      <p className="mx-auto text-center w-[40%] text-[rgba(255, 255, 255, 0.70)] mt-7 leading-6 mb-8">
         Discover the cutting-edge technologies, visionary entrepreneurs, and
         future projects that are propelling Pakistan towards a brighter
         tomorrow.
       </p>
 
       <div className="flex gap-4 justify-center  mt-4">
-        <button className="h-7 font-semibold text-sm w-40 rounded-xl bg-[#009F30]">
+        <button className="h-8 font-semibold text-sm w-40 rounded-full bg-[#009F30]">
           Explore Innovations
         </button>
-        <button className="h-7 text-[14px] w-[12.3rem] border-[0.656px] font-semibold border-[rgba(255, 255, 255, 0.20)] gap-1 rounded-xl flex justify-center items-center">
+        <button className="h-8 text-[14px] w-[12.3rem] border-[0.656px] font-semibold border-[rgba(255, 255, 255, 0.20)] gap-1 rounded-full flex justify-center items-center">
           <p>Meet Our Entrepreneurs</p>
           <Image src="/Arrow.svg" alt="Arrow" width={20} height={20} />
         </button>
